7dd22f40cd10221ba40fee1a696192d105f45c0d
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-09-01 10:58:54 +0800

    removed unnecessary code leftover from previous project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const startingText = 'Heading\n=======\n\nSub-heading\n-----------\n\n### Another deeper heading\n\nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**,\n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Greg Wong](http://www.gregwong.me)*';
-
 export default class App extends React.Component {
 
   constructor(props){
